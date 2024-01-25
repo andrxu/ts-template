af137e0c46cbb39397c8e3e331f9f154842db65c
Andrew Xu <xuwd@hotmail.com>
2024-01-25 23:26:42 +0900

    more minor updates

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,2 @@
 const message: string = 'Hello, TypeScript!';
 console.log(message);
-
-
---
+++ test/unit/app.test.ts
@@ -0,0 +1,5 @@
+describe('add function', () => {
+    test('adds two numbers', () => {
+        expect(true).toBe(true);
+    });
+});
